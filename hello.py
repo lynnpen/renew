84aba55179826c3462210d10921916a6b1ecf2c2
hanlin <l_penny@163.com>
2016-03-29 09:36:59 +0800

    change some thing

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, redirect, render_template, url_for, session, flash, g
-from flask.ext.script import Manager
+#from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
 from flask.ext.wtf import Form
@@ -10,14 +10,14 @@ from flask.ext.socketio import SocketIO, emit, join_room, leave_room, close_room
 from wtforms import StringField, BooleanField, PasswordField, SubmitField
 from wtforms.validators import Required
 from werkzeug.security import generate_password_hash, check_password_hash
-from datetime import datetime
+from datetime import datetime, timedelta
 import os, time, eventlet
-#import logging
+import logging
 
 async_mode = 'eventlet'
 
 
-#logging.basicConfig(filename='message.log',level=logging.DEBUG, filemode = 'w', format = '%(asctime)s - %(levelname)s: %(message)s')
+logging.basicConfig(filename='message.log',level=logging.DEBUG, filemode = 'w', format = '%(asctime)s - %(levelname)s: %(message)s')
 basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'wocawoca'
@@ -26,7 +26,7 @@ app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 db = SQLAlchemy(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
-manager = Manager(app)
+#manager = Manager(app)
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
@@ -40,6 +40,9 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
     password_hash = db.Column(db.String(128))
+  
+    def __repr__(self):
+        return '<User %r>' % self.username
 
     @property
     def password(self):
@@ -65,6 +68,12 @@ class LoginForm(Form):
     password = PasswordField('password', validators=[Required()])
     remember_me = BooleanField('remember me', default = False)
     submit = SubmitField('Log In')
+
+class ModPassword(Form):
+    oldpassword = PasswordField('old password', validators=[Required()])
+    newpassword = PasswordField('new password', validators=[Required()])
+    new2password = PasswordField('repeat new password', validators=[Required()])
+    submit = SubmitField('comfirm')
 
 @login_manager.user_loader
 def load_user(userid):
@@ -97,25 +106,44 @@ def index():
     return render_template('index.html', name=name)
 
 
-@app.route('/modpass')
+@app.route('/modpass', methods=['GET', 'POST'])
 @login_required
 def modpass():
-    pass
+    form = ModPassword()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=current_user.username).first()
+        new1 = form.newpassword.data
+        new2 = form.new2password.data
+        if user.verify_password(form.oldpassword.data) and new1 != '' and new2 != '':
+            if new1 != new2:
+                flash('please check your input')
+            elif new1 == new2:
+                user = User.query.filter_by(username=current_user.username).first()
+                user.password = new1
+                user.password_hash
+                db.session.delete(user)
+                db.session.add(user)
+                db.session.commit()
+                flash('modify password success!')
+    return render_template('mod.html', form=form)
 
 @app.route('/logout')
 def logout():
+    user = User.query.filter_by(username=current_user.username).first()
+    login_user(user, True)
     logout_user()
     flash('You have been logged out.')
     return redirect(url_for('login'))
 
-@app.route('/test')
-def test():
-    return render_template('test.html')
+#@app.route('/test')
+#def test():
+#    return render_template('index.html')
 
 @socketio.on('my event', namespace='/chat')
 def message(message):
     if message['data']:
         emit('my response', {'data': '%s: %s' % (current_user.username, message['data'])}, broadcast=True)
+        logging.debug(current_user.username + ": " + message['data'])
 
 @socketio.on('connect', namespace='/chat')
 def connect():
@@ -136,4 +164,5 @@ def default_error_handler(e):
 
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', debug=True)
+    socketio.run(app)
+    #socketio.run(app, host='0.0.0.0', port=80, debug=True)
--- mod_db.py
+++ mod_db.py
@@ -3,10 +3,10 @@ db.drop_all()
 db.create_all()
 a = User()
 a.password = '123'
-hanlin = User(id=1, username='hanlin', password_hash=a.password_hash)
+hanlin = User(id=1, username='test1', password_hash=a.password_hash)
 db.session.add(hanlin)
 db.session.commit()
-lilijuan = User(id=2, username='lilijuan', password_hash=a.password_hash)
+lilijuan = User(id=2, username='test2', password_hash=a.password_hash)
 db.session.add(lilijuan)
 db.session.commit()
 
